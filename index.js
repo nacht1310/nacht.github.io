7880418c6123b14a53201173ce71b1c1cba1d1e9
Nacht <ducvt1310@gmail.com>
2023-07-27 17:21:34 +0700

    Finish Intro and Info Section

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-const changPage = () => {
-    window.location.href = 'about.html'
+$(document).ready(() => {
+    $('#header').load('header.html', () => {
+        $('li').each((index, element) => {
+            const id = $(element).attr('id').split('-')[0]
+            $(element).on('click',() => {
+                moveToSection(id)
+            })
+        })
+        checkActive()
+    })
+    $('#intro').load('introduction.html')
+    $('#information').load('info.html')
+    $('#experience').load('exp.html')
+    $('#contact').load('contact.html')
+})
+
+$(window).scroll(() => {
+    checkActive()
+})
+
+const checkActive = () => {
+    $('#main>div').each((index, element) => {
+        var wP = $(window).scrollTop()
+        var hT = $(element).offset().top
+        var hB = $(element).outerHeight()
+        if(wP >= hT - 1 && wP <= hT + hB - 1) {
+            $(`#${$(element).attr('id')}-button`).addClass('active')
+        }
+        else {
+            $(`#${$(element).attr('id')}-button`).removeClass('active')
+        }
+    })
 }
 
-const button = document.getElementById('button')
-button.addEventListener('click', changPage)
+const moveToSection = (id) => {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(`#${id}`).offset().top
+        }, 500)
+}
